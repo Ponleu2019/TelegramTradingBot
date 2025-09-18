5ce051b71fb00adbda4aaaf6b498302c06569c3b
Ponleu2019 <48830052+Ponleu2019@users.noreply.github.com>
2025-09-18 21:34:30 +0700

    Update auto_bot.py

--- auto_bot.py
+++ auto_bot.py
@@ -134,7 +134,7 @@ def format_market_message(prices, title="💹 Live Market Prices"):
             message += f"{symbol} {coin}/USD: ${price:,.2f}{arrow}\n"
         else:
             message += f"⚠️ {coin}/USD: N/A{arrow}\n"
-    message += "\n🚨 Trade Safely!"
+    message += "\n💰 Trade Smart. Grow Together 💸"
     return message
 
 # =========================
@@ -221,4 +221,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
